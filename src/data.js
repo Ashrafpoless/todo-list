c0b722eb84372bbe88b4f7202be57dc3dd0d91f7
Ashrafpoless <150967649+Ashrafpoless@users.noreply.github.com>
2025-03-17 19:13:29 +0100

    data

---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data = {
+    items: [
+        {
+            id: 1,
+            text: 'buy bread',
+        },
+        {
+            id: 2,
+            text: 'buy milk',
+        },
+        {
+            id: 3,
+            text: 'buy vegetables',
+        },
+        {
+            id: 4,
+            text: 'buy fruits',
+        },
+    ],
+    id: 5,
+};
+
+export default data;
---
+++ src/init.js
@@ -0,0 +1,8 @@
+import loadEvent from './Events/loadEvent.js'
+import addItemEvent from './Events/addItemEvent.js';
+import clearAllEvent from './Events/clearAllEvent.js';
+
+
+loadEvent();
+addItemEvent();
+clearAllEvent();
